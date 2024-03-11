e3129307e7333442884357dc46e7df7304050ba0
OliverkNilsson <oliverknilsson@gmail.com>
2024-03-11 13:15:54 +0100

    RestController

--- GritAcademyAPI/src/main/java/com/example/GritAcademyAPI/courses/CoursesController.java
+++ GritAcademyAPI/src/main/java/com/example/GritAcademyAPI/courses/CoursesController.java
@@ -3,7 +3,9 @@ package com.example.GritAcademyAPI.courses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CoursesController {
     @Autowired
     CoursesService coursesService;
