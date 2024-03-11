2665a4f9a944930be0ca8f0ccfda55921540e28a
OliverkNilsson <oliverknilsson@gmail.com>
2024-03-11 15:13:21 +0100

    studentsCourses annotations tillagt

--- GritAcademyAPI/src/main/java/com/example/GritAcademyAPI/courses/CoursesDTO.java
+++ GritAcademyAPI/src/main/java/com/example/GritAcademyAPI/courses/CoursesDTO.java
@@ -5,7 +5,7 @@ import com.example.GritAcademyAPI.students.StudentsDTO;
 import java.util.List;
 
 public class CoursesDTO {
-    private Integer id;
+    private Long id;
     private String name;
 
     private String description;
--- GritAcademyAPI/src/main/java/com/example/GritAcademyAPI/studentsCourses/StudentsCoursesRepository.java
+++ GritAcademyAPI/src/main/java/com/example/GritAcademyAPI/studentsCourses/StudentsCoursesRepository.java
@@ -1,9 +1,8 @@
 package com.example.GritAcademyAPI.studentsCourses;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentsCoursesRepository extends CrudRepository<StudentsCourses, Integer> {
-
+public interface StudentsCoursesRepository extends JpaRepository<StudentsCourses, Integer> {
 }
